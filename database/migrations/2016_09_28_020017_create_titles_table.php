719835a10e900162e49d0b74a28a349fedd0fc6d
Ismail Ashour <dr.ashoor@gmail.com>
2016-09-28 04:21:45 +0200

    Import Books And Titles in Route

---
+++ database/migrations/2016_09_28_020017_create_titles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTitlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('titles', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('page')->default(0);
+            $table->integer('level')->default(0);
+            $table->integer('sub')->default(0);
+            
+            $table->string('title');
+
+            $table->integer('book_id')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('titles');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,93 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('import_books', function(){
+
+	// Get All Books Tables in db, Where not books Table.
+	$tables = DB::select('SHOW TABLES 
+		WHERE 
+		(
+		`Tables_in_'.env('DB_DATABASE').'` LIKE "b%" OR 
+		`Tables_in_'.env('DB_DATABASE').'` LIKE "t%" 
+		)
+		AND 
+		(
+		`Tables_in_'.env('DB_DATABASE').'` NOT LIKE "books" AND 
+		`Tables_in_'.env('DB_DATABASE').'` NOT LIKE "titles"
+		);');
+
+	foreach ($tables as $table) {
+    	foreach ($table as $value){
+
+        	import_book_title_tables($value);
+    	}
+	}	
+});
+
+function import_book_title_tables($value){
+
+	// Get BookID
+	if (strstr($value, 'b' ))
+		$table = explode('b', $value);
+	elseif (strstr($value, 't'))
+		$table = explode('t', $value);
+
+	$book_id = $table[1];
+
+	if (\App\Book::whereBookId($book_id)->count()){
+		
+		// Drop Table if existed in Books Table
+		Illuminate\Support\Facades\Schema::dropIfExists($value);
+
+	} else {
+
+		// Retreive All Pages.
+		$pages = DB::table($value)->get();
+
+		foreach ($pages as $page) {
+
+			$shamela_book = new App\Book();
+			
+			$shamela_book->part = $page->part;
+			$shamela_book->page = $page->page;
+			$shamela_book->seal = $page->seal;
+			$shamela_book->text = $page->nass;
+			$shamela_book->book_id = $book_id;
+			
+			$shamela_book->save();
+		}
+
+		// Drop Table After Insertion
+		Illuminate\Support\Facades\Schema::dropIfExists($value);
+	}
+
+	$title_table = 't' . $book_id;
+
+	// Drop Table If Existed in Database
+	if (\App\Title::whereBookId($book_id)->count()){
+		Illuminate\Support\Facades\Schema::dropIfExists($title_table);
+	} else {
+		// Retreive All Pages.
+		$rows = DB::table($title_table)->get();
+
+		foreach ($rows as $row) {
+
+			$shamela_title = new App\Title();
+			
+			$shamela_title->page = $row->id;
+			$shamela_title->level= $row->lvl;
+			$shamela_title->sub = $row->sub;
+			$shamela_title->title = $row->tit;
+			$shamela_title->book_id = $book_id;
+			
+			$shamela_title->save();
+		}
+
+		Illuminate\Support\Facades\Schema::dropIfExists($title_table);
+	}
+
+
+
+	
+}
